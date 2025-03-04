18593082739dfd3cd47deb6d3ced0cd4df63e66d
gt-yordanos <gt.yordanos@gmail.com>
2025-03-04 16:49:50 +0300

    make the nav fixed at top and make the hero to be responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,18 +3,18 @@ import HeroImg from '../assets/ECON.png';
 const Hero = () => {
   return (
     <div className="hero bg-base-200 min-h-screen px-[10%]">
-    <div className="hero-content flex-col lg:flex-row-reverse">
-      <div>
+    <div className="hero-content flex-col lg:flex-row-reverse gap-15 sm:mt-0 mt-16">
+      <div className='flex flex-col items-center rounded-lg shadow-2xl'>
         <img
         src={HeroImg}
-        className="max-w-sm rounded-lg shadow-2xl" />
-        <h2 className='text-center text-4xl font-bold mt-4 mb-2'>HUESA</h2>
-        <p className='text-sm'>
+        className="max-w-sm" />
+        <h2 className='text-center text-2xl sm:text-3xl font-bold mt-4 mb-2'>HUESA</h2>
+        <p className='text-sm px-2 mb-2 text-center'>
           Haramaya University Economics Students Association
         </p>
       </div>
       <div>
-        <h1 className="text-5xl font-bold">Welcome to HUESA!</h1>
+        <h1 className="sm:text-5xl text-3xl font-bold">Welcome to HUESA!</h1>
         <p className="py-6">
           At HUESA, we are dedicated to fostering the growth of students by providing the resources,
           training, and mentorship they need to succeed. We believe in the power of collaboration 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { FaHome, FaInfoCircle, FaBook, FaRegCalendarAlt, FaUserPlus } from "reac
 import ThemeToggle from "./ThemeToggle";
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100 shadow-sm px-4 lg:px-[10%]">
+    <div className="navbar bg-base-100 shadow-sm px-4 lg:px-[10%] fixed">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
