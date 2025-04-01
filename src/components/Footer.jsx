7bbcb1a3eddba57714f8a073e5b620168416a429
gt-yordanos <gt.yordanos@gmail.com>
2025-04-01 14:16:05 +0300

    changed the duplicated facebook link to x/twitter in the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -45,11 +45,11 @@ const Footer = () => {
             </a>
           )}
 
-          {contact.facebook && (
-            <a href={contact.facebook} target="_blank" rel="noopener noreferrer">
-              {/* Facebook Icon */}
-              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 24 24" className="fill-current hover:scale-110 transition-transform duration-200">
-                <path d="M12,2C6.477,2,2,6.477,2,12c0,5.013,3.693,9.153,8.505,9.876V14.65H8.031v-2.629h2.474v-1.749c0-2.896,1.411-4.167,3.818-4.167c1.153,0,1.762,0.085,2.051,0.124v2.294h-1.642c-1.022,0-1.379,0.969-1.379,2.061v1.437h2.995l-0.406,2.629h-2.588v7.247C18.235,21.236,22,17.062,22,12C22,6.477,17.523,2,12,2z"></path>
+          {contact.x && (
+            <a href={contact.x} target="_blank" rel="noopener noreferrer">
+              {/* X Icon */}
+              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 50 50" className="fill-current hover:scale-110 transition-transform duration-200">
+                <path d="M 11 4 C 7.134 4 4 7.134 4 11 L 4 39 C 4 42.866 7.134 46 11 46 L 39 46 C 42.866 46 46 42.866 46 39 L 46 11 C 46 7.134 42.866 4 39 4 L 11 4 z M 13.085938 13 L 21.023438 13 L 26.660156 21.009766 L 33.5 13 L 36 13 L 27.789062 22.613281 L 37.914062 37 L 29.978516 37 L 23.4375 27.707031 L 15.5 37 L 13 37 L 22.308594 26.103516 L 13.085938 13 z M 16.914062 15 L 31.021484 35 L 34.085938 35 L 19.978516 15 L 16.914062 15 z"></path>
               </svg>
             </a>
           )}
